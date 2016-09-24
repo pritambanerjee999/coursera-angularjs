c6801d28828126594f7c080004df89b315feaf87
Banerjee, Pritam (ETW - FLEX) <Pritam.Banerjee@nike.com>
2016-09-23 18:27:30 -0700

    assignment 2

--- assignment2/js/app.js
+++ assignment2/js/app.js
@@ -1,12 +1,33 @@
 (function () {
 	'use strict';
 
-	angular.module('LunchCheck', [])
-	.controller('LunchCheckController', LunchCheckController);
+	angular.module('ShoppingListCheckOff', [])
+	.controller('ToBuyShoppingController', ToBuyShoppingController)
+	.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController);
 
-	LunchCheckController.$inject = ['$scope'];
+	ToBuyShoppingController.$inject = ['$scope'];
+	AlreadyBoughtShoppingController.$inject = ['$scope'];
 
-	function LunchCheckController($scope){
+	function ToBuyShoppingController($scope){
+		$scope.name = "";
+		$scope.totalValue = 0;  
+		$scope.lunchBox = "Enter the lunch name";
+		$scope.lunchMessage = "";
+
+		$scope.tooMuchLunchCheck = function(){
+			var lunchString = $scope.lunchBox;
+			var lunchItems = lunchString.split(",");
+
+			if (lunchItems.length>3){
+				$scope.lunchMessage = "Too much!"
+			}else{
+				$scope.lunchMessage = "Enjoy!"
+			}
+		}
+	}
+	
+	
+	function AlreadyBoughtShoppingController($scope){
 		$scope.name = "";
 		$scope.totalValue = 0;  
 		$scope.lunchBox = "Enter the lunch name";
