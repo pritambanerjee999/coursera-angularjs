6a5445b3dc604159869dfd8199d66af180c014a0
Banerjee, Pritam (ETW - FLEX) <Pritam.Banerjee@nike.com>
2016-10-10 10:10:45 -0700

    test

---
+++ assignment4/categories/categories.controller.js
@@ -0,0 +1,34 @@
+(function(){
+'use strict';
+
+angular.module('data')
+.controller('CategoriesListController', CategoriesListController);
+
+
+CategoriesListController.$inject = ['MenuDataService'];
+
+function CategoriesListController(MenuDataService) {
+	 var menu = this;
+
+	  var promise = MenuDataService.getAllCategories();
+
+	  promise.then(function (response) {
+	    menu.categories = response.data;
+	  })
+	  .catch(function (error) {
+	    console.log("Something went terribly wrong.");
+	  });
+
+	  menu.logMenuItems = function (shortName) {
+	    var promise = MenuCategoriesService.getMenuForCategory(shortName);
+
+	    promise.then(function (response) {
+	      console.log(response.data);
+	    })
+	    .catch(function (error) {
+	      console.log(error);
+	    })
+	  };
+}
+
+})();
--- assignment4/categories/menudata.service.js
+++ assignment4/categories/menudata.service.js
@@ -1,18 +1,34 @@
 (function(){
 	angular.module('data')
-	.service('MenuDataService', MenuDataService);
+	.service('MenuDataService', MenuDataService)
+	.constant('hostName',"https://davids-restaurant.herokuapp.com");
 	
-	function MenuDataService(){
+	MenuDataService.$inject = ['$http', 'hostName']
+	function MenuDataService($http, ApiBasePath){
 		service = this;
 		
 		// https://davids-restaurant.herokuapp.com/categories.json
 		service.getAllCategories = function(){
 			
+			var response = $http({
+			      method: "GET",
+			      url: (hostName + "/categories.json")
+			    });
+
+			    return response;
 		}
 		
 		// https://davids-restaurant.herokuapp.com/menu_items.json?category=
 		service.getItemsForCategory = function(categoryShortName){
-			
+			var response = $http({
+			      method: "GET",
+			      url: (hostName + "/categories.json"),
+			      params: {
+			          category: categoryShortName
+			        }
+			    });
+
+			    return response;
 		}
 	}
 })();
